57ac413ce0298590c493ad7cc46084566266a10b
Mariemgr22 <mariemgargouri998@gmail.com>
2023-01-16 10:31:37 +0100

    navbar

---
+++ client/src/components/Navbar/Navbar.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import {
+  Nav,
+  NavLink,
+  NavMenu,
+  Image
+} from './NavbarElements';
+import logo from '../../images/logo.jpg';
+
+
+const Navbar = () => {
+  return (
+    <Nav>
+      <NavLink>
+        <Image src={logo} />
+      </NavLink>
+      <NavMenu>
+        <NavLink to='/'>
+          Home
+        </NavLink>
+        <NavLink to ='/register'>
+          Register
+        </NavLink>
+        <NavLink to='/login'>
+          Login
+        </NavLink>
+      </NavMenu>
+    </Nav>
+  )
+}
+
+export default Navbar
