92dda8c1ed3e663b26536ab4b39dcc65c52ae350
MortadhaOuertani <mortadha500@gmail.com>
2023-01-16 10:40:25 +0100

    newcommit

--- serverside/app.js
+++ serverside/app.js
@@ -2,6 +2,9 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+require('dotenv').config();
+var cors= require("cors");
+const mongoose = require("mongoose")
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -13,6 +16,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+mongoose.connect(process.env.MONGO_URI)
+.then(()=>console.log('DB CONNECTED'))
+.catch(err=>console.log(err.message))
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
