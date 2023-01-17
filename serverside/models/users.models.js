1d41a0218f1fa135f66ec7ef3906b5d4170911f0
MortadhaOuertani <mortadha500@gmail.com>
2023-01-17 12:32:22 +0100

    finishing the basicsss

--- serverside/models/users.models.js
+++ serverside/models/users.models.js
@@ -1,20 +1,16 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const UserModel = new Schema(
-  {
-    name: "string",
-    email: {
-      type: "string",
-      trim: true,
-      unique: true,
-    },
-    password: "string",
-    role: "string",
-  },
-  {
-    timestamps: true,
-  }
-);
+const UserModel = new Schema({
+    Email: {
+        type: "string",
+        trim: true,
+        unique: true,
+      },
+    Nom : "string",
+    Password: "string",
+    Logo:"string",
+    Role:"string",
+}, {timestamps: true})
 
-module.exports = mongoose.model("users", UserModel);
+
