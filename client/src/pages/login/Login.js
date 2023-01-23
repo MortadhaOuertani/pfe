6c7abeff7fed31ac64111e6e9e4a664cce662246
MortadhaOuertani <mortadha500@gmail.com>
2023-01-23 14:21:16 +0100

    login page

--- client/src/pages/login/Login.js
+++ client/src/pages/login/Login.js
@@ -1,10 +1,30 @@
 import React from 'react'
-import { BB, Container, FForm, FormContainer, Input, InputContainer, OtherInputContainer } from './LoginElements'
-import background from '../../images/login.jpg'
+import { Container,InputBtn, Form, FormContainer, H1, Header, Img, Input, Left, Right, Span } from './LoginElements'
+import login from '../../images/login.svg'
 const Login = () => {
   return (
     <>
-      <Container><FormContainer></FormContainer></Container>
+      <Container><FormContainer>
+        <Left>
+          <Header>
+            <H1>Login</H1>
+          </Header>
+          <Form>
+
+            <Input type='email' required placeholder='Email' />
+            <br/>
+            <Input type='password' required placeholder='Password' />
+            <br/>
+            <InputBtn type="submit" value="Login"/>
+
+          </Form>
+
+        </Left>
+        <Right>
+          <Img src={login}></Img>
+          <p><Span></Span></p>
+        </Right>
+      </FormContainer></Container>
     </>
   )
 }
