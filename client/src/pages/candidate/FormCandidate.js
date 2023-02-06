da461bb4b060da82d8a59870ef4a8fc936025159
Mariemgr22 <mariemgargouri998@gmail.com>
2023-02-06 09:25:51 +0100

    aaa

--- client/src/pages/candidate/FormCandidate.js
+++ client/src/pages/candidate/FormCandidate.js
@@ -1,20 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import { LoginAction, RegistrationCandidate } from '../../redux/actions/authActions'
 import { BtnSubmit, Form, Input } from './FormCandidateElements'
 
 const FormCandidate = () => {
+    const [form, setForm] = useState({})
+    const dispatch = useDispatch()
+    const errors = useSelector(state => state.errors)
+    const navigate = useNavigate()
+    const onChangeHandler = (e) => {  //déclaration d'un event de nom onChangeHandler pour détecter les changements de chaque input
+        setForm({
+            ...form, //setForm va prendre la formulaire(form)
+            [e.target.name]: e.target.value //elle va prendre la valeur d'un input à partir le nom de l'input
+        })
+    }
+    const onSubmit = (e) => { //un event qui va envoyer les données au base de données 
+        e.preventDefault(); //ne refraichir pas la page pour ne perdre pas les données 
+        dispatch(RegistrationCandidate(form, navigate))//appeler la fonction loginAction qui se trouve dans le store 
+    }
     return (
-        <Form>
-            <Input type='name' name='name' placeholder='Your first name' required />
-            <Input type='name'name='lastname' placeholder='Your last name' required />
-            <Input type='email' name='email'  placeholder='Your Email' required />
-            <Input type='password' name='password' placeholder='Password' required />
-            <Input type='password' name='confirm' placeholder='Confirm password' required />
-            <Input type='phone'name='phone' placeholder='Your mobile number' required />
-            <Input type='text' name='niveauEtude' placeholder='Your level of study' required />
-            <Input type='address' name='address' placeholder='Your address' required />
-            <Input type='date' name='anneeObtentienDiplome' placeholder='year of graduation' required />
-            <Input type='text' name='diplome' placeholder='diploma' required />
-            <Input type='text' name='age' placeholder='age' required />
+        <Form onSubmit={onSubmit}>
+            <Input onChange={onChangeHandler}type='name' name='name' placeholder='Your first name' required />
+            <Input  onChange={onChangeHandler}type='name' name='lastname' placeholder='Your last name' required />
+            <Input onChange={onChangeHandler}type='email' name='email' placeholder='Your Email' required />
+            <Input onChange={onChangeHandler}type='password' name='password' placeholder='Password' required />
+            <Input onChange={onChangeHandler} type='password' name='confirm' placeholder='Confirm password' required />
+            <Input onChange={onChangeHandler} type='phone' name='phone' placeholder='Your mobile number' required />
+            <Input onChange={onChangeHandler} type='text' name='niveauEtude' placeholder='Your level of study' required />
+            <Input onChange={onChangeHandler} type='address' name='address' placeholder='Your address' required />
+            <Input onChange={onChangeHandler} type='date' name='anneeObtentienDiplome' placeholder='year of graduation' required />
+            <Input onChange={onChangeHandler} type='text' name='diplome' placeholder='diploma' required />
+            <Input onChange={onChangeHandler} type='text' name='age' placeholder='age' required />
             <BtnSubmit type="submit" >Register</BtnSubmit>
         </Form>
     )
--- client/src/redux/actions/authActions.js
+++ client/src/redux/actions/authActions.js
@@ -3,10 +3,10 @@ import { ERRORS, SET_USER } from "../types"
 import jwt_decode from 'jwt-decode'
 import { setAuth } from "../../util/setAuth"
 
-export const Registration = (form, navigate) => dispatch => {
-    axios.post('http://localhost:3600/api/register').then(res => {
+export const RegistrationCandidate = (form, navigate) => dispatch => {
+    axios.post('http://localhost:3600/api/register/candidate',form).then(res => {
         navigate('/')
-        dispatch({ type: ERRORS, payload: {} })
+        dispatch({ type:ERRORS, payload: {} })
     }).catch(err => {
         dispatch({
             type: ERRORS,
@@ -14,7 +14,19 @@ export const Registration = (form, navigate) => dispatch => {
 
         })
     })
-}
+} 
+export const RegistrationCompany = (form, navigate) => dispatch => {
+    axios.post('http://localhost:3600/api/register/company',form).then(res => {
+        navigate('/')
+        dispatch({ type:ERRORS, payload: {} })
+    }).catch(err => {
+        dispatch({
+            type: ERRORS,
+            payload: err.response.data
+
+        })
+    })
+} 
 export const LoginAction = (form, navigate)=>dispatch  => {
     axios.post('http://localhost:3600/api/login', form, {
         headers: {
@@ -23,10 +35,10 @@ export const LoginAction = (form, navigate)=>dispatch  => {
     })
         .then(res => {
             navigate('/')
-            const { token } = res.data //met le token (backend) dans un variable appelée token(frontend) de type objet {}
-            localStorage.setItem('jwt', token)//met le token dans localstorage (browser) clé : jwt   valeur : token
-            const decode = jwt_decode(token)//décriptage de token pour accéder aux email,name,...
-            dispatch(setUser(decode))//envoyer le nom,email,...
+            const { token } = res.data
+            localStorage.setItem('jwt', token)
+            const decode = jwt_decode(token)
+            dispatch(setUser(decode))
             setAuth(token)
         })
         .catch(err => {
