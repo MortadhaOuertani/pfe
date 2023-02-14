1282e8559f91d44818dcb37fd6bce3be38df8c7d
MortadhaOuertani <mortadha500@gmail.com>
2023-02-14 15:02:31 +0100

    z

--- serverside/models/offers.models.js
+++ serverside/models/offers.models.js
@@ -2,23 +2,18 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const OffersModule = new Schema({
-  company:String, //clé étrangère de la table mère user 
-<<<<<<< HEAD
-    dateExpiration:Date,
-=======
-    dateE:Date,
-    dateD:Date,
->>>>>>> a0b0a8f412390e28fa27261ac6e671d8055db51b
-    nbrRecrute:Number,
-    contract:String,
-    salary:String,
-    study:String,
-    language:String,
-    experience:String,
-    title:String,
-    descripion:String,
-    search:[[]],
+  company: String, //clé étrangère de la table mère user 
+  dateExpiration: Date,
+  nbrRecrute: Number,
+  contract: String,
+  salary: String,
+  study: String,
+  language: String,
+  experience: String,
+  title: String,
+  descripion: String,
+  search: [[]],
 
-  }, {timestamps: true})
+}, { timestamps: true })
 
 module.exports = mongoose.model("offers", OffersModule);
