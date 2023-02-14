52a9d800e127d8b98bdfd2651c2dcc6b7a4a4c49
Mariemgr22 <mariemgargouri998@gmail.com>
2023-02-14 14:58:33 +0100

    msg

--- client/src/pages/offerspage/offer/offer.js
+++ client/src/pages/offerspage/offer/offer.js
@@ -5,46 +5,47 @@ import { Button, Container, LeftSide, MiddleSide, RightSide } from './offerEleme
 
 
 const Offer = ({ experience, _id, company }) => {
-    const [modalShow, setModalShow] = useState(false);
-    const [resultdate, setResultdate] = useState(false);
-    const [companydata, setCompanydata] = useState([])
-    useEffect(() => {
-        GetOfferDate()
-        GetCompanyData(company)
+  const [modalShow, setModalShow] = useState(false);
+  const [resultdate, setResultdate] = useState(false);
+  const [companydata, setCompanydata] = useState([])
+  useEffect(() => {
+    GetOfferDate()
+    GetCompanyData(company)
 
-    }, [])
+  }, [])
 
-    const GetCompanyData = (id) => {
-        axios
-            .get(`http://localhost:3600/api/companydata/${id}`)
-            .then(res => {
+  const GetCompanyData = (id) => {
+    axios
+      .get(`http://localhost:3600/api/companydata/${id}`)
+      .then(res => {
 
-                setCompanydata(res.data)
-            })
-    }
-    const GetOfferDate = () => {
-        axios
-            .get("http://localhost:3600/api/date")
-            .then(res => {
-                setResultdate(res.data.result)
-            })
-            .catch(err => {
-                console.log(err.message)
-            });
-    }
-    return (
-        <>
-            <Container>
-                <Link to={`/offers/${_id}`}> <LeftSide>{experience}</LeftSide></Link>
-                <MiddleSide></MiddleSide>
-                <RightSide>
-                    {resultdate ? <p>klll</p> : null}
-                    <Button variant="primary" onClick={() => setModalShow(true)}>
-                        Voir plus
-                    </Button>
-                    {companydata.map(item => ( <p key={item.id}>{item.email}</p> ))}  </RightSide>
-            </Container>
-        </>)
+        setCompanydata(res.data)
+      })
+  }
+  const GetOfferDate = () => {
+    axios
+      .get("http://localhost:3600/api/date")
+      .then(res => {
+        setResultdate(res.data.result)
+      })
+      .catch(err => {
+        console.log(err.message)
+      });
+  }
+  return (
+    <>
+      <Container>
+         <LeftSide>{experience}</LeftSide>
+        <MiddleSide></MiddleSide>
+        <RightSide>
+          {resultdate ? <p>klll</p> : null}
+          <Link to={`/offers/${_id}`}>
+            <Button variant="primary" onClick={() => setModalShow(true)}>
+              Voir plus
+            </Button></Link>
+          {companydata.map(item => (<p key={item.id}>{item.email}</p>))}  </RightSide>
+      </Container>
+    </>)
 }
 
 export default Offer
--- serverside/controllers/offer.controllers.js
+++ serverside/controllers/offer.controllers.js
@@ -7,7 +7,7 @@ const Addoffers = async (req, res) => {
         req.body.company = req.user.id //creation d'un champ company et l"effectuer l'id de la company qui a crée l'offre(req.user.id from passport)
         OffersModule.create(req.body, (err, data) => {
             if (err) res.status(400).json(err.message)
-         else  {res.status(200).json({ message: "success" })}
+            else { res.status(200).json({ message: "success" }) }
 
         })
     } catch (error) {
@@ -20,11 +20,23 @@ const FindAlloffers = (req, res) => {
         OffersModule.find({}, async (err, data) => {
             if (err) res.status(400).json({ message: err.message })
             else {
-                const company = await companyModel.findOne({ _id: data.company })//await is used to return a true or false//retourner les donnée de la company qui crée les offres
-               data.company=company
+                company = await companyModel.findOne({ _id: data.company },)//await is used to return a true or false//retourner les donnée de la company qui crée les offres
+                console.log(company)
+                data.company = company
                 res.status(200).json(data)//met les donnnée de company dans le objet data
             }
         })
+    } catch (error) {
+        res.status(404).json(error.message)
+    }
+}
+
+const GetCompanyData = (req, res) => { //
+    try {
+      companyModel.find({_id:req.params.id}, (err, data) =>{
+        if (err) res.status(404).json({ message: "not found" })
+        else{ res.status(200).json(data)}
+      })
     } catch (error) {
         res.status(404).json(error.message)
     }
@@ -36,7 +48,7 @@ const FindSingleoffers = (req, res) => {
             if (err) res.status(404).json({ message: "not found" })
             else {
                 const company = companyModel.findOne({ _id: data.company })
-                data.company=company
+                data.company = company
                 res.status(200).json(data)
             }
         })
@@ -57,9 +69,25 @@ const Deleteoffers = async (req, res) => {
     }
 }
 
+const FindDate = (req, res) => {
+    try {
+        companyModel.findOne({ _id: req.user.id }, (err, data) => {
+            if (err) res.status(404).json(err.message)
+
+            if (data) res.status(200).json({ result: true })
+            if (!data) res.status(200).json({ result: false })
+
+        })
+    } catch (err) {
+        res.status(404).json(err.message)
+    }
+}
+
 module.exports = {
     Addoffers,
     FindAlloffers,
     FindSingleoffers,
-    Deleteoffers
+    Deleteoffers,
+    FindDate,
+    GetCompanyData
 }
