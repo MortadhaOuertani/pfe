9676305b7e8b1f755c343e601e8dfc3ab2f3ce0f
Mariemgr22 <mariemgargouri998@gmail.com>
2023-02-20 10:37:58 +0100

    mmml

--- serverside/controllers/users.controllers.js
+++ serverside/controllers/users.controllers.js
@@ -2,7 +2,8 @@ const UserModel = require("../models/users/users.models");
 const CompanyModel = require("../models/users/company.model");
 const ValidateLogin = require("../validation/Login")
 const jwt = require('jsonwebtoken')
-const bcrypt = require('bcryptjs')
+const bcrypt = require('bcryptjs');
+const adminModel = require("../models/users/admin.model");
 
 const RegisterCandidate = async (req, res) => {
   try {
@@ -124,19 +125,72 @@ const LoginCompany = async (req, res) => {
   }
 }
 
-const Test = (req, res) => {
-  res.send("welcome user")
+const RegisterAdmin = (req, res) => {
+  try {
+    adminModel.findOne({ email: req.body.email }).then(async (exist) => {
+      if (exist) {
+        errors.email = "user exist";
+        res.status(404).json(errors);
+      } else {
+        const salt = bcrypt.genSaltSync(10)
+        const hash = bcrypt.hashSync(req.body.password, salt)//hashed password
+        req.body.password = hash;
+        req.body.role = "ADMIN";
+        await adminModel.create(req.body);
+        res.status(200).json({ message: "success" });
+      }
+    });
+  }
+  catch (error) {
+    res.status(404).json(error.message);
+  }
 }
 
-const Admin = (req, res) => {
-  res.send("welcome admin")
+const LoginAdmin = (req, res) => {
+  const { errors, isValid } = ValidateLogin(req.body)
+  try {
+    if (!isValid) {
+      res.status(404).json(errors)
+    } else {
+      adminModel.findOne({ email: req.body.email })
+        .then(user => {
+          if (!user) {
+            errors.email = "user not found "
+            res.status(404).json(errors)
+
+          } else {
+            bcrypt.compare(req.body.password, user.password)
+              .then(isMatch => {
+                if (!isMatch) {
+                  errors.password = "incorrect password"
+                  res.status(404).json(errors)
+                } else {
+                  var token = jwt.sign({
+                    id: user._id,
+                    name: user.name,
+                    email: user.email,
+                    role: user.role
+                  }, "HDYHHSY6", { expiresIn: '1h' });
+                  res.status(200).json({
+                    message: "success",
+                    token: "Bearer " + token
+                  })
+                }
+              })
+          }
+        })
+    }
+  } catch (error) {
+    res.status(404).json(error.message);
+  }
 }
+
 
 module.exports = {
   RegisterCandidate,
   RegisterCompany,
   LoginCandidate,
   LoginCompany,
-  Test,
-  Admin
+  RegisterAdmin,
+  LoginAdmin
 };
